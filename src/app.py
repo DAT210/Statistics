87bbdc7224f2c5cc1dbf557a11c05aaf859e2c22
Mats Tyldum <ma.tyldum@gmail.com>
2018-10-17 20:41:48 +0200

    app.py update
    Implemented server side extraction of data from db to be used in charts and tables.

--- src/app.py
+++ src/app.py
@@ -6,7 +6,8 @@ Statistics: Python/flask app
 
 from flask import Flask, render_template, request, redirect, url_for, session, g, send_from_directory, jsonify
 import mysql.connector
-
+import json
+import collections
 
 app = Flask(__name__)
 app.debug = True  # only for development!
@@ -18,7 +19,6 @@ app.config["DATABASE_DB"] = "statistics"
 app.config["DATABASE_HOST"] = "localhost"
 
 app.secret_key = "secret_key"
-
 
 # Get connection to db
 def get_db():
@@ -35,29 +35,89 @@ def teardown_db(error):
     if db is not None:
         db.close()
 
-
-# Routes for front end client
+# Front end client routes and functions
 @app.route("/statistics/")
 def index():
     return render_template("index.html")
 
 @app.route("/statistics/charts/")
 def charts():
-   return render_template("charts.html")
+    return render_template("charts.html", orders_per_month=orders_per_month(), 
+        orders_per_dish=orders_per_dish())
 
 @app.route("/statistics/tables/")
-def tables():
-    return render_template("tables.html")
+def tables(): 
+    return render_template("tables.html", orders=get_all_orders(), customers=get_all_customers(), 
+        dishes=get_all_dishes())
 
-# API routes
-@app.route("/statistics/orders/")
-def orders():
-    return "/statistics/orders/"
+def orders_per_month():
+    orders = get_all_orders()
+    res = {}
+    for order in orders:
+        curr_month = order["date"].month
+        if curr_month in res:
+            res[curr_month] += 1
+        else:
+            res[curr_month] = 1
 
-@app.route("/statistics/orders/<int:order_id>/")
-def show_order(order_id):
-    order = get_order(order_id)
-    return jsonify(order)
+    res = collections.OrderedDict(sorted(res.items()))  # Sort result based on month number
+
+    months = {1: "January", 2: "February", 3: "Mars", 4: "April", 5: "May", 6: "June", 
+    7: "July", 8: "August", 9: "September", 10: "October", 11: "November", 12: "December"}
+
+    res_names = {}  # New dict with month names instead of numbers
+    for num, count in res.items():
+        month = months[num]   
+        res_names[month] = count
+    return json.dumps(res_names)
+
+def orders_per_dish():
+    orders = get_all_orders()
+    dish_lookup = get_dish_lookup_dict()
+    res = {}
+
+    for order in orders:
+        curr_dish_id = order["dish_id"]
+        if curr_dish_id in res:
+            res[curr_dish_id] += 1
+        else:
+            res[curr_dish_id] = 1
+   
+    res_names = {}  # New dict with dish name instead of dish id
+    for id, count in res.items():
+        dish_name = dish_lookup[id]
+        res_names[dish_name] = count
+
+    res_names = collections.OrderedDict(sorted(res_names.items()))  # Sort result based on dish name
+    return json.dumps(res_names)
+
+def get_all_orders():
+    db = get_db()
+    cur = db.cursor()
+    try:
+        sql = "SELECT * FROM orders"
+        cur.execute(sql)
+
+        orders = []
+        for order in cur.fetchall():
+            new = {
+                "order_id": order[0],
+                "timestamp": order[1],
+                "date": order[2],
+                "order_type": order[3],
+                "customer_id": order[4],
+                "dish_id": order[5],
+                "delivery": order[6],
+                "price": order[7]
+            }
+            orders.append(new)
+        if len(orders) <= 0:
+            print("No orders in our database")
+    except mysql.connector.Error as err:
+        print("Oops, something went wrong:", err)
+    finally:
+        cur.close()
+    return orders
 
 def get_order(order_id):
 
@@ -73,8 +133,8 @@ def get_order(order_id):
         else:
             order_info = {
                 "order_id": order[0],
-                "timestamp": order[1],
-                "timestamp": order[2],
+                "time_stamp": order[1],
+                "date": order[2],
                 "order_type": order[3],
                 "customer_id": order[4],
                 "dish_id": order[5],
@@ -88,16 +148,70 @@ def get_order(order_id):
         cur.close()
     return order_info
 
-@app.route("/statistics/customers/")
-def customers():
-    return "/statistics/customers/"
+def get_all_dishes():
+    db = get_db()
+    cur = db.cursor()
+    try:
+        sql = "SELECT * FROM dish"
+        cur.execute(sql)
 
+        dishes = []
+        for dish in cur.fetchall():
+            new = {
+                "dish_id": dish[0],
+                "dish_name": dish[1],
+                "price": dish[2]
+            }
+            dishes.append(new)
+        if len(dishes) <= 0:
+            print("No dishes in our database")
+    except mysql.connector.Error as err:
+        print("Oops, something went wrong:", err)
+    finally:
+        cur.close()
+    return dishes
 
-@app.route("/statistics/customers/<int:customer_id>/")
-def show_customer(customer_id):
-    customer = get_customer(customer_id)
-    #return render_template("show_customer.html", customer=customer)
-    return jsonify(customer)
+def get_dish(dish_id):
+    db = get_db()
+    cur = db.cursor()
+    try:
+        sql = "SELECT * FROM dish WHERE dish_id=%s"
+        cur.execute(sql, (dish_id, ))
+        dish = cur.fetchone()
+        if dish == None:
+            raise NameError("This dish_id doesn\'t exist in the database")
+        else:
+            dish_info = {
+                "dish_id": dish[0],
+                "dish_name": dish[1],
+                "price": dish[2]
+            }
+
+    except mysql.connector.Error as err:
+        print("Oops, something went wrong:", err)
+    finally:
+        cur.close()
+    return dish_info
+
+def get_dish_lookup_dict():
+    db = get_db()
+    cur = db.cursor()
+    try:
+        sql = "SELECT * FROM dish"
+        cur.execute(sql)
+
+        dishes = {}
+        for dish in cur.fetchall():
+            dish_id, dish_name = dish[0], dish[1]
+            dishes[dish_id] = dish_name
+
+        if len(dishes) <= 0:
+            print("No dishes in our database")
+    except mysql.connector.Error as err:
+        print("Oops, something went wrong:", err)
+    finally:
+        cur.close()
+    return dishes
 
 def get_customer(customer_id):
     db = get_db()
@@ -126,37 +240,58 @@ def get_customer(customer_id):
         cur.close()
     return customer_info
 
+def get_all_customers():
+    db = get_db()
+    cur = db.cursor()
+    try:
+        sql = "SELECT * FROM customer"
+        cur.execute(sql)
+
+        customers = []
+        for customer in cur.fetchall():
+            new = {
+                "customer_id": customer[0],
+                "f_name": customer[1],
+                "s_name": customer[2],
+                "phone": customer[3],
+                "birthdate": customer[4],
+                "email": customer[5],
+                "c_address": customer[6]
+            }
+            customers.append(new)
+        if len(customers) <= 0:
+            print("No dishes in our database")
+    except mysql.connector.Error as err:
+        print("Oops, something went wrong:", err)
+    finally:
+        cur.close()
+    return customers
+
+# API routes
+@app.route("/statistics/orders/")
+def orders():
+    return "/statistics/orders/"
+
+@app.route("/statistics/orders/<int:order_id>/")
+def show_order(order_id):
+    order = get_order(order_id)
+    return jsonify(order)
+
+@app.route("/statistics/customers/")
+def customers():
+    return "/statistics/customers/"
+
+@app.route("/statistics/customers/<int:customer_id>/")
+def show_customer(customer_id):
+    return jsonify(get_customer(customer_id))
+
 @app.route("/statistics/dish/")
 def dishes():
     return "/statistics/dish/"
 
 @app.route("/statistics/dish/<int:dish_id>/")
 def show_dishes(dish_id):
-    dish = get_dish(dish_id)
-    return jsonify(dish)
-
-def get_dish(dish_id):
-    db = get_db()
-    cur = db.cursor()
-    try:
-        sql = "SELECT * FROM dish WHERE dish_id=%s"
-        cur.execute(sql, (dish_id, ))
-
-        dish = cur.fetchone()
-        if dish == None:
-            raise NameError("This dish_id doesn\'t exist in the database")
-        else:
-            dish_info = {
-                "dish_id": dish[0],
-                "dish_name": dish[1],
-                "price": dish[2]
-            }
-
-    except mysql.connector.Error as err:
-        print("Oops, something went wrong:", err)
-    finally:
-        cur.close()
-    return dish_info
+    return jsonify(get_dish(dish_id))
 
 # Error handlers 
 @app.errorhandler(404)
