285bc37b617bec89c14f8c28cbba4d9d04c288a5
Mats Tyldum <ma.tyldum@gmail.com>
2018-10-17 20:42:55 +0200

    Charts update
    Charts now showing real data extracted from db: Orders per month & Orders per dish.

--- src/static/js/charts.js
+++ src/static/js/charts.js
@@ -1,118 +1,159 @@
+// Global variables to store chart data
+var ordersPerMonth, ordersPerMonthData, ordersPerMonthLabels;
+var ordersPerDish, ordersPerDishData, ordersPerDishLabels;
+
+// Init function. Labels and data processed in the same order as present in JSON object
+function sendData(ordersMonth, ordersDish) {
+    ordersPerMonth = JSON.parse(ordersMonth);
+    console.log(ordersPerMonth)
+    
+    ordersPerMonthData = [];
+    ordersPerMonthLabels = [];
+    for (var key in ordersPerMonth) {
+        ordersPerMonthLabels.push(key);
+        ordersPerMonthData.push(ordersPerMonth[key]);
+    }
+
+    ordersPerDish = JSON.parse(ordersDish);
+    console.log(ordersPerDish)
+
+    ordersPerDishData = [];
+    ordersPerDishLabels = [];
+    for (var key in ordersPerDish) {
+        ordersPerDishLabels.push(key);
+        ordersPerDishData.push(ordersPerDish[key]);
+    }
+    console.log(ordersPerDishData);
+}
+
 var random = function random() {
     return Math.round(Math.random() * 100);
 };
-var lineChart = new Chart($('#canvas-1'), {
-    type: 'line',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgba(220, 220, 220, 0.2)',
-            borderColor: 'rgba(220, 220, 220, 1)',
-            pointBackgroundColor: 'rgba(220, 220, 220, 1)',
-            pointBorderColor: '#fff',
-            data: [random(), random(), random(), random(), random(), random(), random()]
-        }, {
-            label: 'My Second dataset',
-            backgroundColor: 'rgba(151, 187, 205, 0.2)',
-            borderColor: 'rgba(151, 187, 205, 1)',
-            pointBackgroundColor: 'rgba(151, 187, 205, 1)',
-            pointBorderColor: '#fff',
-            data: [random(), random(), random(), random(), random(), random(), random()]
-        }]
-    },
-    options: {
-        responsive: true
-    }
-});
-var barChart = new Chart($('#canvas-2'), {
-    type: 'bar',
-    data: {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-        datasets: [{
-            backgroundColor: 'rgba(220, 220, 220, 0.5)',
-            borderColor: 'rgba(220, 220, 220, 0.8)',
-            highlightFill: 'rgba(220, 220, 220, 0.75)',
-            highlightStroke: 'rgba(220, 220, 220, 1)',
-            data: [random(), random(), random(), random(), random(), random(), random()]
-        }, {
-            backgroundColor: 'rgba(151, 187, 205, 0.5)',
-            borderColor: 'rgba(151, 187, 205, 0.8)',
-            highlightFill: 'rgba(151, 187, 205, 0.75)',
-            highlightStroke: 'rgba(151, 187, 205, 1)',
-            data: [random(), random(), random(), random(), random(), random(), random()]
-        }]
-    },
-    options: {
-        responsive: true
-    }
-});
-var doughnutChart = new Chart($('#canvas-3'), {
-    type: 'doughnut',
-    data: {
-        labels: ['Red', 'Green', 'Yellow'],
-        datasets: [{
-            data: [300, 50, 100],
-            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-            hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
-        }]
-    },
-    options: {
-        responsive: true
-    }
-});
-var radarChart = new Chart($('#canvas-4'), {
-    type: 'radar',
-    data: {
-        labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
-        datasets: [{
-            label: 'My First dataset',
-            backgroundColor: 'rgba(220, 220, 220, 0.2)',
-            borderColor: 'rgba(220, 220, 220, 1)',
-            pointBackgroundColor: 'rgba(220, 220, 220, 1)',
-            pointBorderColor: '#fff',
-            pointHighlightFill: '#fff',
-            pointHighlightStroke: 'rgba(220, 220, 220, 1)',
-            data: [65, 59, 90, 81, 56, 55, 40]
-        }, {
-            label: 'My Second dataset',
-            backgroundColor: 'rgba(151, 187, 205, 0.2)',
-            borderColor: 'rgba(151, 187, 205, 1)',
-            pointBackgroundColor: 'rgba(151, 187, 205, 1)',
-            pointBorderColor: '#fff',
-            pointHighlightFill: '#fff',
-            pointHighlightStroke: 'rgba(151, 187, 205, 1)',
-            data: [28, 48, 40, 19, 96, 27, 100]
-        }]
-    },
-    options: {
-        responsive: true
-    }
-});
-var pieChart = new Chart($('#canvas-5'), {
-    type: 'pie',
-    data: {
-        labels: ['Red', 'Green', 'Yellow'],
-        datasets: [{
-            data: [300, 50, 100],
-            backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
-            hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
-        }]
-    },
-    options: {
-        responsive: true
-    }
-});
-var polarAreaChart = new Chart($('#canvas-6'), {
-    type: 'polarArea',
-    data: {
-        labels: ['Red', 'Green', 'Yellow', 'Grey', 'Blue'],
-        datasets: [{
-            data: [11, 16, 7, 3, 14],
-            backgroundColor: ['#FF6384', '#4BC0C0', '#FFCE56', '#E7E9ED', '#36A2EB']
-        }]
-    },
-    options: {
-        responsive: true
-    }
-});
+
+$(document).ready(function(){
+    var lineChart = new Chart($('#canvas-1'), {
+        type: 'line',
+        data: {
+            labels: ordersPerMonthLabels,
+            datasets: [{
+                label: '# of orders',
+                backgroundColor: 'rgba(151, 187, 205, 0.2)',
+                borderColor: 'rgba(151, 187, 205, 1)',
+                pointBackgroundColor: 'rgba(151, 187, 205, 1)',
+                pointBorderColor: '#fff',
+                data: ordersPerMonthData
+            }]
+        },
+        options: {
+            responsive: true,
+        }
+    });
+    var doughnutChart = new Chart($('#canvas-2'), {
+        type: 'doughnut',
+        data: {
+            labels: ['Red', 'Green', 'Yellow'],
+            datasets: [{
+                data: [300, 50, 100],
+                backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+                hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+            }]
+        },
+        options: {
+            responsive: true,
+        }
+    });
+    var barChart = new Chart($('#canvas-3'), {
+        type: 'bar',
+        data: {
+            labels: ordersPerDishLabels,
+            datasets: [{
+                label: "# of orders",
+                backgroundColor: 'rgba(220, 220, 220, 0.5)',
+                borderColor: 'rgba(220, 220, 220, 0.8)',
+                highlightFill: 'rgba(220, 220, 220, 0.75)',
+                highlightStroke: 'rgba(220, 220, 220, 1)',
+                data: ordersPerDishData
+            }]
+        },
+        options: {
+            responsive: true,
+            scales: {
+                xAxes: [{   // In order to show disable auto skip feature so all labels are visible
+                    stacked: false,
+                    beginAtZero: true,
+                    scaleLabel: {
+                        labelString: 'Month'
+                    },
+                    ticks: {
+                        stepSize: 1,
+                        min: 0,
+                        autoSkip: false
+                    }
+                }],
+                yAxes: [{   // Set y-axis to begin at zero
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+    barChart.update();
+    var radarChart = new Chart($('#canvas-4'), {
+        type: 'radar',
+        data: {
+            labels: ['Eating', 'Drinking', 'Sleeping', 'Designing', 'Coding', 'Cycling', 'Running'],
+            datasets: [{
+                label: 'My First dataset',
+                backgroundColor: 'rgba(220, 220, 220, 0.2)',
+                borderColor: 'rgba(220, 220, 220, 1)',
+                pointBackgroundColor: 'rgba(220, 220, 220, 1)',
+                pointBorderColor: '#fff',
+                pointHighlightFill: '#fff',
+                pointHighlightStroke: 'rgba(220, 220, 220, 1)',
+                data: [65, 59, 90, 81, 56, 55, 40]
+            }, {
+                label: 'My Second dataset',
+                backgroundColor: 'rgba(151, 187, 205, 0.2)',
+                borderColor: 'rgba(151, 187, 205, 1)',
+                pointBackgroundColor: 'rgba(151, 187, 205, 1)',
+                pointBorderColor: '#fff',
+                pointHighlightFill: '#fff',
+                pointHighlightStroke: 'rgba(151, 187, 205, 1)',
+                data: [28, 48, 40, 19, 96, 27, 100]
+            }]
+        },
+        options: {
+            responsive: true
+        }
+    });
+    /*
+    var pieChart = new Chart($('#canvas-5'), {
+        type: 'pie',
+        data: {
+            labels: ['Red', 'Green', 'Yellow'],
+            datasets: [{
+                data: [300, 50, 100],
+                backgroundColor: ['#FF6384', '#36A2EB', '#FFCE56'],
+                hoverBackgroundColor: ['#FF6384', '#36A2EB', '#FFCE56']
+            }]
+        },
+        options: {
+            responsive: true
+        }
+    });
+    var polarAreaChart = new Chart($('#canvas-6'), {
+        type: 'polarArea',
+        data: {
+            labels: ['Red', 'Green', 'Yellow', 'Grey', 'Blue'],
+            datasets: [{
+                data: [11, 16, 7, 3, 14],
+                backgroundColor: ['#FF6384', '#4BC0C0', '#FFCE56', '#E7E9ED', '#36A2EB']
+            }]
+        },
+        options: {
+            responsive: true
+        }
+    });
+    */
+})
