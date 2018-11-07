5462f3f5698b4989aa75a080567533480a7a532f
Chrystallic <stiansbjaaland@hotmail.com>
2018-11-07 12:27:48 +0100

    Initial work done on function showing bookings_per_restaurant. added a small shellscript for db_initialization(each person might need to chmod it, if they want to run it

--- src/statistics_functions/booking_functions.py
+++ src/statistics_functions/booking_functions.py
@@ -31,3 +31,60 @@ def get_booking(booking_id):
     finally:
         cur.close()
     return json.dumps(booking_info)
+
+
+def booking_per_restaurant():
+    db = app.get_db()
+    cur = db.cursor()
+    try:
+        sql = "SELECT DISTINCT restaurant_id FROM booking ORDER BY restaurant_id;"
+        cur.execute(sql)
+        booking = []
+        for restaurant_id in cur.fetchall():
+            sql_booking_per_restaurant = "SELECT COUNT(*) FROM booking WHERE restaurant_id = %s;"
+            cur.execute(sql_booking_per_restaurant, (restaurant_id))
+            total_bookings = cur.fetchone()
+            new = {
+                "restaurant_id": restaurant_id[0],
+                "total_bookings": total_bookings[0]
+            }
+            booking.append(new)
+        print(booking)
+        if len(booking) <= 0:
+            print("No bookings in our database")
+    except mysql.connector.Error as err:
+        print("Oops, something went wrong:", err)
+    finally:
+        cur.close()
+    return json.dumps(booking)
+
+
+def ingredients_per_restaurant_stock():
+    db = app.get_db()
+    cur = db.cursor()
+    try:
+        sql = "SELECT DISTINCT restaurant_id FROM booking ORDER BY restaurant_id;"
+        cur.execute(sql)
+        stock = []
+        for restaurant_id in cur.fetchall():
+            sql_ingredient_list_per_restaurant = "SELECT ingredient_id, quantity FROM stock WHERE restaurant_id = %s;"
+            cur.execute(sql_ingredient_list_per_restaurant, (restaurant_id))
+            total_stock = cur.fetchone()
+            new = {}
+            for ingredient in total_stock:
+                sql_ingredient_name = "SELECT ingredient_name FROM ingredient WHERE ingredient_id = %s;"
+                ingredient_name = cur.execute(sql_ingredient_name, (ingredient[0]))
+
+                new [ingredient[0]] = {
+                    "ingredient_name": ingredient_name,
+                    "quantity": ingredient[1]
+                }
+            stock.append(new)
+        print(stock)
+        if len(stock) <= 0:
+            print("No stocks in our database")
+    except mysql.connector.Error as err:
+        print("Oops, something went wrong:", err)
+    finally:
+        cur.close()
+    return 0
